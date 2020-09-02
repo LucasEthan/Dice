4d4762679d8eacd430b9f177947ebdc518b4b77d
Lucas Ethan <asmrit1010@gmail.com>
2020-09-02 10:33:40 +0200

    Made it so you can puts your number in a variable

--- dice.rb
+++ dice.rb
@@ -1,11 +1,11 @@
 class Dice
-attr_reader :number_of_rolls
+attr_reader :sides
 
   def initialize(sides)
     @sides = sides
   end
 
   def roll(number_of_rolls)
-      puts rand(2..@sides) * number_of_rolls
+    rand(2..sides) * number_of_rolls
   end
 end
