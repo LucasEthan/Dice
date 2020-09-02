fd8d9ce07ee7d53385fab77685731a3438ecb406
Lucas Ethan <asmrit1010@gmail.com>
2020-09-02 10:16:18 +0200

    Improved the code

--- dice.rb
+++ dice.rb
@@ -1,16 +1,11 @@
 class Dice
-attr_reader :timesroll
+attr_reader :number_of_rolls
 
   def initialize(sides)
     @sides = sides
-    @timesroll = timesroll
   end
-  def roll(timesroll)
-    if timesroll == 2 
-    puts rand(2..12)
-    end
-    if timesroll == 6 
-      puts rand(3..60)
-    end
+
+  def roll(number_of_rolls)
+      puts rand(2..@sides) * number_of_rolls
   end
 end
