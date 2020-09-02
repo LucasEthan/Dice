da8faf9c86b90bd94a24266efb264c3b9f2cc613
Lucas Ethan <asmrit1010@gmail.com>
2020-09-02 11:28:47 +0200

    Fixed the code

--- dice.rb
+++ dice.rb
@@ -5,7 +5,9 @@ attr_reader :sides
     @sides = sides
   end
 
-  def roll(number_of_rolls)
-    rand(2..sides) * number_of_rolls
+  def roll(number_of_rolls = 1)
+    roll = 0 # accumulator
+    number_of_rolls.times { roll += rand(1..sides) }
+    roll
   end
 end
