bc0af3bd830334c7bc9afd3c9e3e4fdb4f44e4c4
Lucas Ethan <asmrit1010@gmail.com>
2020-09-22 12:35:11 +0200

    Added loops

--- craps_game.rb
+++ craps_game.rb
@@ -1,16 +1,21 @@
 #!/usr/bin/env ruby
 
-# require "craps_helper"
-# require "craps"
-# include CrapsHelper
+require "craps_helper"
+require "craps"
 
-craps = Craps.new
-craps.play
-craps.rolls
-display_win_or_lose(craps.win_state)
+include CrapsHelper
 
-def display_rolls
+loop do
+  craps = Craps.new
+  puts "Prees ENTER to play craps"
+  gets
+  craps.play
   craps.rolls.each do |roll|
-  puts "You rolled #{roll}!"
+    puts "You rolled #{roll}!"
   end
+  display_win_or_lose(craps.win_state)  
+  puts "Do you want to play again? [y, n]"
+  choice = gets.chomp.upcase
+
+  break if choice == "Y"
 end
