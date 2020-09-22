970c369f056e8a375562617fdc438c6f93987cda
Lucas Ethan <asmrit1010@gmail.com>
2020-09-22 12:46:06 +0200

    Bug fixes

--- craps_game.rb
+++ craps_game.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/env ruby
 
-require "craps_helper"
-require "craps"
+require_relative "craps_helper"
+require_relative "craps"
 
 include CrapsHelper
 
 loop do
   craps = Craps.new
-  puts "Prees ENTER to play craps"
+  puts "Press ENTER to play craps"
   gets
   craps.play
   craps.rolls.each do |roll|
     puts "You rolled #{roll}!"
   end
   display_win_or_lose(craps.win_state)  
-  puts "Do you want to play again? [y, n]"
+  print "Do you want to play again? [y, n]: "
   choice = gets.chomp.upcase
 
-  break if choice == "Y"
+  break if choice == "N"
 end
