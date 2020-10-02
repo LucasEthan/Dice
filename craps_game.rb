674916c4ff60ba84ee879e4217c626165e8372c5
Lucas Ethan <asmrit1010@gmail.com>
2020-10-02 12:21:31 +0200

    Refactoring to use methods

--- craps_game.rb
+++ craps_game.rb
@@ -15,9 +15,7 @@ loop do
   begin
     puts "You have #{personal_funds.balance_in_euros}"
     print "How much do you want to bet: "
-    bet = Integer(gets)
-    raise(BetError, "You must enter a positive bet") unless bet.positive?
-    raise(BetError, "You can't enter a bet more than #{personal_funds.balance_in_euros}") if bet > personal_funds.balance_in_euros
+    bet = get_bet
     
   rescue ArgumentError
     puts "You must input a valid bet"
@@ -26,12 +24,13 @@ loop do
     puts e.message
     retry
   end
-  craps = Craps.new
+
+  craps = Craps.new  
   puts "\nPress ENTER to play craps"
   gets
   craps.play
   craps.rolls.each do |roll|
-    puts "You rolled #{roll}!"
+  puts "You rolled #{roll}!"
   end
   display_win_or_lose(craps.win_state)
   display_score(Craps)
@@ -45,6 +44,7 @@ loop do
     # convert bet to cents before deducting from personalFunds
     personal_funds.deduct(bet * 100)
   end
+
   if !personal_funds.zero_balance?
     print "Do you want to play again? [y, n]: "
     choice = gets.chomp.upcase
--- craps_helper.rb
+++ craps_helper.rb
@@ -1,4 +1,6 @@
 module CrapsHelper
+
+
   def display_win_or_lose(win_state)
     if win_state
       puts "You win"
@@ -11,5 +13,14 @@ module CrapsHelper
     puts "\n***** Number of games: #{craps.number_of_games} *****"
     puts "***** Number of wins: #{craps.wins} *****"
     puts "***** Number of losses: #{craps.losses} *****"
+  end
+
+  def pause_game
+  end
+
+  def get_bet
+    bet = Integer(gets)
+    raise(BetError, "You must enter a positive bet") unless bet.positive?
+    raise(BetError, "You can't enter a bet more than #{personal_funds.balance_in_euros}") if bet > personal_funds.balance_in_euros
   end
 end
