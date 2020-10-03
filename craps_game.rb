bdb7ad49dcc479b436e6612be1e3052a0b205011
Lucas Ethan <asmrit1010@gmail.com>
2020-10-03 11:32:45 +0200

    Refactored the code to use the craps_helper module

--- craps_game.rb
+++ craps_game.rb
@@ -10,10 +10,10 @@ include CrapsHelper
 personal_funds = PersonalFunds.new(100_000)
 loop do
   begin
-    puts "You have #{personal_funds.balance_in_euros}"
+    puts "You have #{personal_funds.balance_in_euros} euros"
     print "How much do you want to bet: "
     bet = input_bet(personal_funds)
-    
+
   rescue ArgumentError
     puts "You must input a valid bet"
     retry
@@ -22,30 +22,12 @@ loop do
     retry
   end
 
-  craps = Craps.new  
-  puts "\nPress ENTER to play craps"
-  gets
-  craps.play
-  craps.rolls.each do |roll|
-  puts "You rolled #{roll}!"
-  end
-  display_win_or_lose(craps.win_state)
-  display_score(Craps)
+  craps = Craps.new
+  start_game(bet, personal_funds, craps)
 
-  if craps.win_state
-    puts "You have recieved #{bet}"
-    # convert bet to cents before adding to personalFunds
-    personal_funds.add(bet * 100)
-  else
-    puts "You have lost #{bet}"
-    # convert bet to cents before deducting from personalFunds
-    personal_funds.deduct(bet * 100)
-  end
 
   if !personal_funds.zero_balance?
-    print "Do you want to play again? [y, n]: "
-    choice = gets.chomp.upcase
-    break if choice == "N"
+    break if play_again_prompt == "N"
   else
     puts "You have no more money. Game Over."
     break
--- craps_helper.rb
+++ craps_helper.rb
@@ -1,6 +1,5 @@
 module CrapsHelper
 
-
   class BetError < StandardError
   end
 
@@ -19,6 +18,8 @@ module CrapsHelper
   end
 
   def pause_game
+    puts "\nPress ENTER to play craps"
+    gets
   end
 
   def input_bet(personal_funds)
@@ -27,4 +28,37 @@ module CrapsHelper
     raise(BetError, "You can't enter a bet more than #{personal_funds.balance_in_euros}") if bet > personal_funds.balance_in_euros
     bet
   end
+
+  def display_rolls(craps)
+    craps.rolls.each do |roll|
+    puts "You rolled #{roll}!"
+    end
+  end
+
+  def update_funds(craps, bet, personal_funds)
+      if craps.win_state
+         puts "You have recieved #{bet} euros"
+         # convert bet to cents before adding to personalFunds
+         personal_funds.add(bet * 100)
+      else
+         puts "You have lost #{bet} euros"
+         # convert bet to cents before deducting from personalFunds
+         personal_funds.deduct(bet * 100)
+      end
+  end
+
+  def play_again_prompt
+    print "Do you want to play again? [y, n]: "
+    choice = gets.chomp.upcase
+  end
+
+  def start_game(bet, personal_funds, craps)
+    pause_game
+    craps.play
+    display_rolls(craps)
+    display_win_or_lose(craps.win_state)
+    display_score(Craps)
+    update_funds(craps, bet, personal_funds)
+  end
+
 end
